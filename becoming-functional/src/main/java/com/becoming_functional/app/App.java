87908281cf4cc1879595cb9f5f884e8949a47736
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2018-05-04 20:53:25 -0300

    Create becoming-functional project with maven archetype

---
+++ becoming-functional/src/main/java/com/becoming_functional/app/App.java
@@ -0,0 +1,13 @@
+package com.becoming_functional.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
