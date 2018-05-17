be3b5726aebe71131b75385546b44401aef09567
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2018-05-16 23:34:42 -0300

    Refactoring with High Order Functions as if condition

--- becoming-functional/README.md
+++ becoming-functional/README.md
@@ -6,13 +6,17 @@
 
 - Anonymous functions are split up into two types, lambda functions and closures.
 
-### Lambda functions
+#### Lambda functions
 
 - Lambda functions are *unnamed* functions that contain a *parameter list*, a *body* and a return.
 
 ![Imagem 2.1](notes/images/Screenshot from 2018-05-04 20-41-54.png)
 ![Imagem 2.2](notes/images/Screenshot from 2018-05-04 20-42-00.png)
 
-### Closures
+#### Closures
 
 - Closures are much like lambda's except they reference variables outside the scope of the function (the *body* references a variable that neither exists in the *body* or the *parameter list*. )
+
+### High Order Functions
+
+- A function becomes “higher order” if it accepts or returns a function.
--- becoming-functional/src/main/java/com/becoming_functional/customer/Customer.java
+++ becoming-functional/src/main/java/com/becoming_functional/customer/Customer.java
@@ -20,52 +20,102 @@ public class Customer {
     public B call(A1 in1);
   }
 
+  static final public Function1<Customer,Boolean> EnabledCustomer = new Function1<Customer,Boolean>() {
+    public Boolean call(Customer customer) {
+      return customer.enabled == true;
+    }
+  };
+
+  static final public Function1<Customer,Boolean> DisabledCustomer = new Function1<Customer,Boolean>() {
+    public Boolean call(Customer customer) {
+      return customer.enabled == false;
+    }
+  };
+
   static private class CustomerAsCustomer implements Function1<Customer, Customer> {
     public String call(Customer customer) { return customer; }
   }
 
   public static List<String> getEnabledCustomerAddresses() {
-    return Customer.getEnabledCustomerField(new Function1<Customer,String>() {
+    return Customer.getField(Customer.EnabledCustomer, new Function1<Customer,String>() {
+      public String call(Customer customer) { return customer.address; }
+    });
+  }
+
+  public static List<String> getDisabledCustomerAddresses() {
+    return Customer.getField(Customer.DisabledCustomer, new Function1<Customer,String>() {
       public String call(Customer customer) { return customer.address; }
     });
   }
 
   public static List<String> getEnabledCustomerDomains() {
-    return Customer.getEnabledCustomerField(new Function1<Customer,String>() {
+    return Customer.getField(Customer.EnabledCustomer, new Function1<Customer,String>() {
+      public String call(Customer customer) { return customer.domain; }
+    });
+  }
+
+  public static List<String> getDisabledCustomerDomains() {
+    return Customer.getField(Customer.DisabledCustomer, new Function1<Customer,String>() {
       public String call(Customer customer) { return customer.domain; }
     });
   }
 
   public static List<String> getEnabledCustomerNames() {
-    return Customer.getEnabledCustomerField(new Function1<Customer,String>() {
+    return Customer.getField(Customer.EnabledCustomer, new Function1<Customer,String>() {
+      public String call(Customer customer) { return customer.name; }
+    });
+  }
+
+  public static List<String> getDisabledCustomerNames() {
+    return Customer.getField(Customer.DisabledCustomer, new Function1<Customer, String>() {
       public String call(Customer customer) { return customer.name; }
     });
   }
 
   public static List<String> getEnabledCustomerPrimaryContacts() {
-    return Customer.getEnabledCustomerField(new Function1<Customer,String>() {
+    return Customer.getField(Customer.EnabledCustomer, new Function1<Customer,String>() {
+      public String call(Customer customer) { return customer.primaryContact; }
+    });
+  }
+
+  public static List<String> getDisabledCustomerPrimaryContacts() {
+    return Customer.getField(Customer.DisabledCustomer, new Function1<Customer,String>() {
       public String call(Customer customer) { return customer.primaryContact; }
     });
   }
 
   public static List<String> getEnabledCustomerStates() {
-    return Customer.getEnabledCustomerField(new Function1<Customer,String>() {
+    return Customer.getField(Customer.EnabledCustomer, new Function1<Customer,String>() {
+      public String call(Customer customer) { return customer.state; }
+    });
+  }
+
+  public static List<String> getDisabledCustomerStates() {
+    return Customer.getField(Customer.DisabledCustomer, new Function1<Customer,String>() {
       public String call(Customer customer) { return customer.state; }
     });
   }
 
   public static List<String> getEnabledCustomerSomeoneEmail(final String someone) {
-    return Customer.getEnabledCustomerField(new Function1<Customer,String>() {
+    return Customer.getField(Customer.EnabledCustomer, new Function1<Customer,String>() {
       public String call(Customer customer) {
         return someone + "@" + customer.domain;
       }
     });
   }
 
-  public static <B> List<B> getEnabledCustomerField(Function1<Customer, B> func) {
+  public static List<String> getDisabledSomeoneEmail(final String someone) {
+    return Customer.getField(Customer.Disabled, new Function1<Customer,String>() {
+      public String call(Customer customer) {
+        return someone + "@" + customer.domain;
+      }
+    });
+  }
+
+  public static <B> List<B> getField(Function1<Customer,Boolean> test, Function1<Customer, B> func) {
     ArrayList<B> outList = new ArrayList<B>();
-    for(Customer customer : Customer.allCustomers) {
-      if (customer.enabled == true) {
+    for (Customer customer : Customer.allCustomers) {
+      if (test.call(customer)) {
         outList.add(func.call(customer));
       }
     }
